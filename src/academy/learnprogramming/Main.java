69b1191e21e8012789f6aaee46496d227edf912e
agusmx <agusmx77@gmail.com>
2022-01-24 17:59:04 -0600

    Añade merge y quick sort

--- src/academy/learnprogramming/Main.java
+++ src/academy/learnprogramming/Main.java
@@ -2,23 +2,27 @@ package academy.learnprogramming;
 
 import java.util.Random;
 
-import static academy.learnprogramming.algorithms.Sorting.*;
+import static academy.learnprogramming.algorithms.Sorting.quickSort;
 
 public class Main {
 
     public static void main(String[] args) {
-
         Random random = new Random();
-        int[] intArray = random.ints(10, -50, 50).toArray();
+        int[] intArray = random.ints(5, -50, 50).toArray();
         System.out.println("Unsorted array: ");
         printArray(intArray);
         System.out.println();
-
-        bubbleSort(intArray);
-        selectionSort(intArray);
-        insertionSort(intArray);
+//        bubbleSort(intArray);
+//        selectionSort(intArray);
+//        insertionSort(intArray);
+        quickSort(intArray);
+//        long inicio = System.currentTimeMillis();
+//        mergeSort(intArray);
+//        long fin = System.currentTimeMillis();
         System.out.println("Sorted array: ");
         printArray(intArray);
+
+
     }
 
     /**
--- src/academy/learnprogramming/algorithms/Sorting.java
+++ src/academy/learnprogramming/algorithms/Sorting.java
@@ -1,5 +1,7 @@
 package academy.learnprogramming.algorithms;
 
+import java.util.Random;
+
 import static academy.learnprogramming.utils.Utils.swap;
 
 public class Sorting {
@@ -8,6 +10,9 @@ public class Sorting {
     /**
      * Implementation of Bubble sort algorithm
      * Stable Sort
+     * <p>
+     * Divide logically into ordered an unordered in-site array.
+     * Swap every largest element to the next one until way up the entire array
      *
      * @param array array to be sorted
      */
@@ -28,9 +33,9 @@ public class Sorting {
      * @param a array to be sorted
      */
     public static void selectionSort(int[] a) {
-        for (int i = a.length - 1; i > 0; i--) {
+        for (int i = a[-1]; i > 0; i--) {
             int l = 0;
-            for (int j = 0; j <= i; j++) {
+            for (int j = 1; j <= i; j++) {
                 if (a[j] > a[l]) {
                     l = j;
                 }
@@ -41,6 +46,7 @@ public class Sorting {
 
     /**
      * Implements insertion sort algorithm
+     * Stable sort
      *
      * @param a array to be sorted
      */
@@ -56,4 +62,69 @@ public class Sorting {
         }
 
     }
+
+    public static void mergeSort(int[] a) {
+        mergeSort(a, 0, a.length);
+    }
+
+    public static void mergeSort(int[] a, int i, int j) {
+        if (j - i < 2) return;
+
+        int m = (i + j) / 2;
+        mergeSort(a, i, m);
+        mergeSort(a, m, j);
+        merge(a, i, m, j);
+    }
+
+
+    public static void merge(int[] a, int s, int m, int e) {
+        if (a[m - 1] <= a[m]) return;
+
+        int i = s;
+        int j = m;
+        int tempIndex = 0;
+
+        int[] tmp = new int[e - s];
+
+        while (i < m && j < e) {
+            tmp[tempIndex++] = a[i] <= a[j] ? a[i++] : a[j++];
+        }
+
+        System.arraycopy(a, i, a, s + tempIndex, m - i);
+        System.arraycopy(tmp, 0, a, s, tempIndex);
+    }
+
+    public static void quickSort(int[] a) {
+        quickSort(a, 0, a.length);
+    }
+
+    public static void quickSort(int[] a, int s, int e) {
+        if (e - s < 1) return;
+        int p = partition(a, s, e);
+        mergeSort(a, s, p);
+        mergeSort(a, p + 1, e);
+    }
+
+    public static int partition(int[] a, int s, int e) {
+        Random r = new Random();
+        int[] x = {a[s], a[s + 1], a[e - 1]};
+        int p = x[r.nextInt(x.length - 1)];
+
+        int i = s;
+        int j = e - 1;
+        while (i < j) {
+            while (i < j && a[j] >= p) {
+                j--;
+            }
+            if (i < j) a[i] = a[j];
+            while (i < j && a[i] <= p) {
+                i++;
+            }
+            if (i < j) a[j] = a[i];
+        }
+        a[j] = p;
+        return j;
+    }
+
+
 }
